3e9773a4c58fbce3031c2d5edb63f383d42dbcf8
Oscar Harron <oscarharron@hotmail.com>
2025-07-17 18:43:22 +1200

    work on gamerestart logic

--- src/store.js
+++ src/store.js
@@ -1,3 +1,7 @@
 import { atom } from 'nanostores';
 
 export const gameStarted = atom(false);
+export const twoPlayer = atom(false);
+export const threePlayer = atom(false);
+export const onePlayer = atom(false);
+export const gameFinished = atom(false);
