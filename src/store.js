6d2120bad9968fd5b999d22e3ef2c3030ac789c0
Oscar Harron <oscarharron@hotmail.com>
2025-07-22 18:14:05 +1200

    add lives counter outside of screen

--- src/store.js
+++ src/store.js
@@ -6,3 +6,6 @@ export const threePlayer = atom(false);
 export const onePlayer = atom(false);
 export const gameFinished = atom(false);
 export const gameOver = atom(false);
+export const p1LivesText = atom(3);
+export const p2LivesText = atom(3);
+export const p3LivesText = atom(3);
