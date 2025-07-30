e53a7cff6d67982b3627ff805b0c9be6be4ca661
Oscar Harron <oscarharron@hotmail.com>
2025-07-30 19:59:20 +1200

    update player 2 and 3 logic before refactor

--- src/store.js
+++ src/store.js
@@ -6,6 +6,7 @@ export const threePlayer = atom(false);
 export const onePlayer = atom(false);
 export const gameFinished = atom(false);
 export const gameOver = atom(false);
+export const numberOfPlayers = atom(1);
 export const p1LivesText = atom(3);
 export const p2LivesText = atom(3);
 export const p3LivesText = atom(3);
